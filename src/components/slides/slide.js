0735f35f5888a817323769e5825a84a16ece7dcc
Yogesh Kumar <kumarryogeshh@gmail.com>
2019-09-12 13:51:11 +0530

    implementing lazy loading

--- src/components/slides/slide.js
+++ src/components/slides/slide.js
@@ -1,30 +1,45 @@
 import React from "react";
-import { Header, Image, Message } from "semantic-ui-react";
+import { Header, Image, Message, Placeholder } from "semantic-ui-react";
+import LazyLoad from "react-lazyload";
+
+const Loading = () => (
+  <Placeholder>
+    <Placeholder.Header image>
+      <Placeholder.Line />
+      <Placeholder.Line />
+    </Placeholder.Header>
+    <Placeholder.Paragraph>
+      <Placeholder.Line />
+      <Placeholder.Line />
+      <Placeholder.Line />
+      <Placeholder.Line />
+    </Placeholder.Paragraph>
+  </Placeholder>
+);
 
 const Slide = ({ slide }) => {
   return (
     <div className="ui container">
-      <div>
-        <Header
-          as="h2"
-          dividing
-          content={slide.title}
-          subheader={slide.tagline}
-        />
-        <Message.List>
-          {slide.description &&
-            slide.description.map((msg, i) => (
-              <Message.Item key={i}>{msg}</Message.Item>
-            ))}
-        </Message.List>
-        <p></p>
-        <Image src={slide.imageUrl} size="big" centered></Image>
-      </div>
-      <br />
-      <br />
-      <br />
+      <LazyLoad placeholder={<Loading />}>
+        <div>
+          <Header
+            as="h2"
+            dividing
+            content={slide.title}
+            subheader={slide.tagline}
+          />
+          <Message.List>
+            {slide.description &&
+              slide.description.map((msg, i) => (
+                <Message.Item key={i}>{msg}</Message.Item>
+              ))}
+          </Message.List>
+          <Image src={slide.imageUrl} size="big" centered></Image>
+        </div>
+        <br />
+        <br />
+      </LazyLoad>
     </div>
   );
 };
-
 export default Slide;
